06dbbcad850c10f1d2e1b6df9fab9c2775979953
rafaelesmundo <88125710+rafaelesmundo@users.noreply.github.com>
2022-04-01 13:32:29 -0400

    added comments to mac_finder.py

--- mac_finder.py
+++ mac_finder.py
@@ -1,13 +1,18 @@
+#Raf
+#mac_finder.py
+#Helps you find the Port a MAC Adress is on with Extreme VOSS,EXOS,AND ERS
+
 from netmiko import ConnectHandler
 import getpass
 
+#Info about the Switch and MAC Adderss
 switch_ip = input("What is the IP address of the switch: ")
 switch_username = input("what is the username: ")
 switch_password = getpass.getpass(prompt = "What is the password: ")
 mac_address = input("What is the MAC address you are looking for\n Example xx:xx:xx:xx:xx:xx : ")
 switch_os_input = input("What type of OS is on the Switch?\n (1)ERS\n (2)EXOS\n (3)VSP\n Please Select a number: ")
 
-
+#Extreme Platforms that we are accessing
 extreme_ers = {
     'device_type': 'extreme_ers',
     'host':   '{}'.format(switch_ip),
@@ -29,7 +34,7 @@ extreme_vsp = {
     'password': '{}'.format(switch_password),
 }
 
-
+#Conditions based on input from user about the os type
 if switch_os_input == "1":
     os_type = extreme_ers
     net_connect = ConnectHandler(**os_type)
@@ -43,6 +48,6 @@ elif:
     net_connect = ConnectHandler(**os_type)
     output = net_connect.send_command("show vlan mac-address-entry mac {}".format(mac_address))
 
-
+#Print the results
 print("\nHere is the Port the MAC address is on:\n")
 print(output)
